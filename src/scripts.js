ef9e7e6884f79aced5b00689cbf3931b218c16b7
Irmak Erdem <irmakerdem9@gmail.com>
2022-06-09 18:00:06 -0600

    Add images

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,7 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/world-travel-logo.png'
+import './images/Cappadocia.jpg'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
