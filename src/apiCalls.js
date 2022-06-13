d6c56cb59c6fd2d1a95e74f9a12f6410f48e2742
Irmak Erdem <irmakerdem9@gmail.com>
2022-06-12 21:36:45 -0600

    Get GET successfully through fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,20 @@
+//QUERY SELECTOR
+let errorMessage = document.querySelector('.errorMessage');
+
 //FETCH REQUESTS
+const getPromise = (url) => {
+  return fetch(url)
+  .then(response => response.json())
+  .catch(error => errorMessage.innerText = '🤡 OOOPSIES! 🤨 There was an error. Please try again. 🤡 ');
+}
+
+const allTravelPromises = getPromise('http://localhost:3001/api/v1/travelers');
+const allTripsPromises = getPromise('http://localhost:3001/api/v1/trips');
+const allDestinationsPromises = getPromise('http://localhost:3001/api/v1/destinations');
+
+export {allTravelPromises, allTripsPromises, allDestinationsPromises}
+
+
 
 
 //POST REQUESTS
--- src/scripts.js
+++ src/scripts.js
@@ -5,42 +5,131 @@ import Trip from './Trip';
 import './css/styles.css';
 import './images/world-travel-logo.png'
 import './images/Cappadocia.jpg'
-// import {fetchAllData, addUserSleepData, addUserActivityData, addUserHydrationData, getSleepData, getHydrationData, getActivityData} from './apiCalls';
+import {allTravelPromises, allTripsPromises, allDestinationsPromises} from './apiCalls';
 
 
 
 //GLOBAL VARIABLES
-let globalTraveler;
-let globalTrip;
-let globalDestination;
+// let globalTraveler;
+// let globalTrip;
+// let globalDestination;
+// let allTravelPromises;
+// let allTripsPromises;
+// let allDestinationsPromises;
 
+
+let allTrips;
+let allTravelers;
+let allDestinations;
+
+
+const displayEverything = () => {
+  Promise.all(
+    [allTravelPromises, allTripsPromises, allDestinationsPromises]
+  )
+  .then(response => {
+    allTravelers = response[0].travelers
+    allTrips = response[1].trips
+    allDestinations = response[2].destinations
+    console.log(allTravelers)
+    console.log(allTrips)
+    console.log(allDestinations)
+  })
+}
 
 
 //QUERY SELECTORS
-let yearlyExpense = document.querySelector('.yearlyExpense');
-
-
+// let yearlyExpense = document.querySelector('.yearlyExpense');
+// let tripEstimate = document.querySelector('.tripEstimate');
+// let getEstimateBtn = document.querySelector('.getEstimateBtn');
 
 //EVENT LISTENERS
-// window.addEventListener('load', displayEverything);
-
+window.addEventListener('load', displayEverything);
+//getEstimateBtn.addEventListener('click', );
 
 
 //API FETCH FUNCTIONS
+// function getAllUserData(data) {
+//   globalUserData = data;
+//   globalUserRepository = new UserRepository(data);
+// }
+
+// function getAllHydrationData(data) {
+//   globalHydrationData = data;
+//   globalHydration = new Hydration(data);
+// }
+
+// function getAllSleepData(data) {
+//   globalSleepData = data;
+//   globalSleep = new Sleep(data);
+//   getUserName();
+// }
+
+// function getAllActivityData(data) {
+//   globalActivityData = data;
+//   globalActivity = new Activity(data, globalUserData);
+// }
+
+// function displayResolvedData() {
+//   fetchAllData()
+//   .then((allData) => {
+//     getAllUserData(allData[0].userData);
+//     getAllHydrationData(allData[1].hydrationData);
+//     getAllActivityData(allData[2].activityData);
+//     getAllSleepData(allData[3].sleepData);
+//   })
+// }
+
+
 
 
 //FUNCTIONS
-const displayYearlyExpense = () => {
-  let travelerSpecificTrips = globalTrip.getTripDataById(globalTraveler.id);
-  let yearlyCost = globalTrip.getTotalSpentThisYear(travelerSpecificTrips);
-  yearlyExpense.innerHTML = `🤑 You've spent $${yearlyCost} this year 🤑`;
-}
+// function show(element) {
+//   element.classList.remove('hidden');
+// };
 
-displayYearlyExpense();
+// function hide(element) {
+//   element.classList.add('hidden');
+// };
 
-// const displayEverything = () => {
-//   displayYearlyExpense();
+
+
+
+// const displayYearlyExpense = () => {
+//   let travelerSpecificTrips = globalTrip.getTripDataById(globalTraveler.id);
+//   let yearlyCost = globalTrip.getTotalSpentThisYear(travelerSpecificTrips);
+//   yearlyExpense.innerHTML = `🤑 You've spent $${yearlyCost} this year 🤑`;
 // }
+// displayYearlyExpense();
+
+
+
+
+// const clearForm = () => {
+//   somethingggg.innerHTML = '';
+// }
+// clearForm();
+
+
+
+
+
+// const displayTripCostEstimate = () => {
+//   let tripCost = globalTrip.getSingleTripCost()
+//   tripEstimate.innerHTML = `Trip Request's Estimate is $${tripCost}*`;
+// }
+// displayTripCostEstimate();
+
+
+
+
+
+// const allData = Promise.all(endpoints).then((value) => {
+//   return value;
+// });
+
+
+
 
 
 
