e43ed3a9a2e64d1b5356d792070f080c2b849a0e
Irmak Erdem <irmakerdem9@gmail.com>
2022-06-12 19:42:43 -0600

    Add pointer cursor to parts of form

--- src/scripts.js
+++ src/scripts.js
@@ -3,22 +3,45 @@ import Traveler from './Traveler';
 import Destination from './Destination';
 import Trip from './Trip';
 import './css/styles.css';
+import './images/world-travel-logo.png'
 import './images/Cappadocia.jpg'
 // import {fetchAllData, addUserSleepData, addUserActivityData, addUserHydrationData, getSleepData, getHydrationData, getActivityData} from './apiCalls';
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
 
 //GLOBAL VARIABLES
+let globalTraveler;
+let globalTrip;
+let globalDestination;
+
+
+
+//QUERY SELECTORS
+let yearlyExpense = document.querySelector('.yearlyExpense');
+
 
 
 //EVENT LISTENERS
+// window.addEventListener('load', displayEverything);
+
 
 
 //API FETCH FUNCTIONS
 
 
 //FUNCTIONS
+const displayYearlyExpense = () => {
+  let travelerSpecificTrips = globalTrip.getTripDataById(globalTraveler.id);
+  let yearlyCost = globalTrip.getTotalSpentThisYear(travelerSpecificTrips);
+  yearlyExpense.innerHTML = `🤑 You've spent $${yearlyCost} this year 🤑`;
+}
+
+displayYearlyExpense();
+
+// const displayEverything = () => {
+//   displayYearlyExpense();
+// }
+
 
 
 //API POST FUNCTIONS
